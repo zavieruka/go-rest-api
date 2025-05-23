f2edb4af49bba7f19b235242ded593a39f96cd63
zavieruka <zvkmateus@gmail.com>
2025-05-22 21:42:08 -0300

    add Docker Compose setup with PostgreSQL and initial database migration

---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,9 @@
+create table personalidades(
+    id serial primary key,
+      name varchar,
+      history varchar
+    );
+
+    INSERT INTO personalidades(name, history) VALUES
+    ('Deodato Petit Wertheimer', 'Deodato Petit Wertheimer was a Brazilian doctor and politician. His early years were in São Paulo, but he soon moved to Nova Friburgo in the State of Rio de Janeiro, and at the age of 11, he joined the Colégio Anchieta of the Jesuits.'),
+    ('Carmela Dutra', 'Carmela Teles Leite Dutra was the First Lady of Brazil from January 31, 1946, until her death, being the wife of Eurico Gaspar Dutra, the 16th President of Brazil. She was affectionately called Dona Santinha for her strong religiosity, leading her husband to open a small chapel in the Palácio Guanabara.');
